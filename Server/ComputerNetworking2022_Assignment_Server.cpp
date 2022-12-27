e0ee3ce2be569e2c34a1f763b05a03478cb71180
TriKang <ysck73@gmail.com>
2022-12-27 20:21:23 +0900

    HTTPAssignment 솔루션 하나만 존재하던 구조에서 Server, Client로 프로젝트 구조를 분리함.

--- Server/ComputerNetworking2022_Assignment_Server.cpp
+++ Server/ComputerNetworking2022_Assignment_Server.cpp
@@ -1,4 +1,4 @@
-﻿// HTTPAssignment.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+﻿// ComputerNetworking2022_Assignment_Server.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
 #include <iostream>
